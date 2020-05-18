35f4b287043ea1ef0fba13a6a10f321eadf7d075
ErvinsZ <63841322+ErvinsZ@users.noreply.github.com>
2020-05-18 14:57:41 +0300

    Update Day1.js

--- Day1.js
+++ Day1.js
@@ -45,10 +45,12 @@ console.log(highestNum)
 
 //Exercise 5
 
-num5 = 30
-num6 = 87
+num5 = 70
+num6 = 89
 
-if(num5 && num6) {
-    
+if((num5 >= 40 && num5 <= 60 && num6 >= 40 && num6 <= 60) ||(num5 >= 70 && num5 <= 100 && num6 >= 70 && num6 <= 100)){
+    console.log("True")
+} else {
+    console.log("False")
 }
 

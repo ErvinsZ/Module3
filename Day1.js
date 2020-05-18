a14beefbf3c666ae9a00964b90681b5f4bcb6682
ErvinsZ <63841322+ErvinsZ@users.noreply.github.com>
2020-05-18 14:48:19 +0300

    Create Day1.js

---
+++ Day1.js
@@ -0,0 +1,54 @@
+//Exercise 1
+let x = 10
+let y = 10
+
+sum = x+y
+
+if(x===y) {
+    console.log("Triple sum: ", sum*3)
+} else {
+    console.log("Sum: ", sum)
+}
+
+//Exercise 2
+
+let num = 50
+let num1 = 2
+
+if(num+num1 === 50 || num===50 || num1===50){
+    console.log("True")
+} else{
+    console.log("False")
+}
+
+//Exercise 3
+
+let string = "Strive"
+let newString = string.replace('i','')
+console.log(newString)
+
+//Exercise 4
+
+let num2 = 59
+let num3 = 10
+let num4 = 3
+
+if (num2>num3){
+    highestNum = num2
+} else {
+    highestNum = num3
+}
+if (num4>highestNum){
+    highestNum = num4
+}
+console.log(highestNum)
+
+//Exercise 5
+
+num5 = 30
+num6 = 87
+
+if(num5 && num6) {
+    
+}
+
